47282f30e4275a2dbf0cde982db4a09facadc9dd
Ian Bailey <IanKulin@kulin.com.au>
2023-10-21 15:27:36 +0800

    syncronous template file reading + tidyups

--- server.js
+++ server.js
@@ -4,29 +4,40 @@ const path = require('path');
 const showdown = require('showdown');
 const converter = new showdown.Converter();
 
-const publicDirectory = 'public'; 
-const staticRoot = path.join(__dirname, publicDirectory);
-const templatePath = path.join(staticRoot, 'template.html'); 
+const publicDirectory = 'public';
+const templateName = 'template.html';
 const hostname = '127.0.0.1';
 const port = 3000;
+const version = 'v0.01';
+
+const staticRoot = path.join(__dirname, publicDirectory);
+const templatePath = path.join(staticRoot, templateName);
 
 let templateData = '';
 let useTemplate = false;
 
 
+// middleware for processing markdown files
 function mdParser(req, res, next) {
-    if (req.url.endsWith('.md')) {  
-        // must be a markdown file 
+    if (req.url.endsWith('.md')) {
         const mdFilePath = path.join(staticRoot, req.url);
-      
+
         fs.readFile(mdFilePath, 'utf8', (err, data) => {
             if (err) {
                 console.log('err');
                 res.status(404).send('File not found');
             } else {
-                // convert to HTML using showDown
                 const htmlContent = converter.makeHtml(data);
-                res.send(htmlContent);
+
+                if (useTemplate) {
+                    // Replace placeholders with title and content using the template loaded at startup
+                    const title = path.basename(mdFilePath);
+                    const templatedHtml = templateData.replace('{{title}}', title).replace('{{content}}', 
+                        htmlContent);
+                    res.send(templatedHtml);
+                } else {
+                    res.send(htmlContent);
+                }
             }
         })
     } else {
@@ -34,29 +45,47 @@ function mdParser(req, res, next) {
     }
 };
 
+
 var app = express();
 app.use(mdParser);
 app.use(express.static(publicDirectory));
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     // if there's no index.html, try index.md
-    req.url = '/index.md'; 
-    mdParser(req, res, () => {});
+    req.url = '/index.md';
+    mdParser(req, res, () => { });
 });
- 
 
-app.listen(port, hostname, function() {
-    console.log(`Reading template...`);
-    fs.readFile(templatePath, 'utf8', (err, data) => {
-        if (err) {
-            useTemplate = false;
-            console.log('No template found at', templatePath);
-        } else {
-            useTemplate = true;
-            templateData = data;
-            console.log('Template loaded from', templatePath);
-        }
+
+// Promisify fs.readFile
+const readFilePromise = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
     });
-    console.log(`Server running at http://${hostname}:${port}/`);
- });
+};
+
+
+console.log('mdserver ', version);
+// Use a promise to read the template file then start the server
+readFilePromise(templatePath)
+    .then((data) => {
+        useTemplate = true;
+        templateData = data;
+        console.log('Template loaded from', templatePath);
+    })
+    .catch((err) => {
+        useTemplate = false;
+        console.log('No template found at', templatePath);
+    })
+    .finally(() => {
+        app.listen(port, hostname, () => {
+            console.log(`Server running at http://${hostname}:${port}/`);
+        });
+    });
