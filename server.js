24d1961b36ea63e270b36bbd43c519ed8a4c702a
Ian Bailey <IanKulin@kulin.com.au>
2023-12-17 07:50:57 +0800

    move globals to config.js

--- server.js
+++ server.js
@@ -5,36 +5,12 @@ const packageJson = require('./package.json');
 const showdown = require('showdown');
 const converter = new showdown.Converter({metadata: true});
 
-const publicDirectory = 'public';
-const templateName = 'template.html';
-const hostname = '0.0.0.0';
-const port = 3000;
-
-const staticRoot = path.join(__dirname, publicDirectory);
-const templatePath = path.join(staticRoot, templateName);
-
-let templateData = '';
-let useTemplate = false;
-
-const welcome_html = `
-<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>mdserver - welcome</title></head>
-<body><main><p>You've successfully installed <strong>mdserver</strong>.</p><p>To use:</p><ul>
-<li>create a <code>public</code> directory inside the directory where you launched the container from</li> 
-<li>put your files in there</li><li>markdown files will be converted to HTML on the fly</li>
-<li>other files will be served normally</li><li>if the file <code>template.html</code> 
-is present, it will be used as a template to enclose the markdown</li>
-</ul><p>Learn more at the project's <a href="https://github.com/IanKulin/mdserver">GitHub page</a></p>
-<h3>sample template.html</h3><code>&lt;!DOCTYPE html&gt;<br>&lt;html lang="en"&gt;<br>
-&lt;head&gt;<br>&ensp; &lt;meta charset="UTF-8"&gt;<br>&ensp; &lt;title&gt;{{title}}&lt;/title&gt;<br>
-&lt;/head&gt;<br>&lt;body&gt;<br>&ensp; &lt;main&gt;<br>&emsp; {{content}}<br>&ensp; &lt;/main&gt;<br>
-&lt;/body&gt;<br>&lt;/html&gt;<br></code><p>(To remove this message, ensure you have a <code>
-public/index.md</code> or <code>public/index.html</code> file installed)</p></main></body></html>`
-
+const config = require('./config');
 
 // middleware for processing markdown files
 function mdParser(req, res, next) {
     if (req.url.toLowerCase().endsWith('.md')) {
-        const mdFilePath = path.join(staticRoot, req.url);
+        const mdFilePath = path.join(config.staticRoot, req.url);
 
         fs.readFile(mdFilePath, 'utf8', (err, data) => {
             if (err) {
@@ -42,7 +18,7 @@ function mdParser(req, res, next) {
                     // if we were looking for /index.md and it's not there, it's probably a 
                     // fresh install so we will return the welcome message
                     if (req.url === '/index.md') {
-                        res.status(200).send(welcome_html);
+                        res.status(200).send(config.welcome_html);
                         return;
                     } else {
                         res.status(404).send('File not found');
@@ -56,7 +32,7 @@ function mdParser(req, res, next) {
             } else {
                 const rawHtml = converter.makeHtml(data);
 
-                if (useTemplate) {
+                if (config.useTemplate) {
                     // Replace placeholders with title and content using the template loaded at startup
                     let title = converter.getMetadata().title;
                     console.log(title);
@@ -64,7 +40,7 @@ function mdParser(req, res, next) {
                         title = path.basename(mdFilePath);
                     }
                     
-                    const templatedHtml = templateData.replace('{{title}}', title).replace('{{content}}',
+                    const templatedHtml = config.templateData.replace('{{title}}', title).replace('{{content}}',
                         rawHtml);
                     res.send(templatedHtml);
                 } else {
@@ -80,7 +56,7 @@ function mdParser(req, res, next) {
 
 const app = express();
 app.use(mdParser);
-app.use(express.static(publicDirectory));
+app.use(express.static(config.publicDirectory));
 
 
 app.get('/', function (req, res) {
@@ -106,18 +82,18 @@ const readFilePromise = (path) => {
 
 // Use a promise to read the template file then start the server
 console.log('Starting mdserver ver', packageJson.version);
-readFilePromise(templatePath)
+readFilePromise(config.templatePath)
     .then((data) => {
-        useTemplate = true;
-        templateData = data;
-        console.log('Template loaded from', templatePath);
+        config.useTemplate = true;
+        config.templateData = data;
+        console.log('Template loaded from', config.templatePath);
     })
     .catch((err) => {
-        useTemplate = false;
-        console.log('No template found at', templatePath);
+        config.useTemplate = false;
+        console.log('No template found at', config.templatePath);
     })
     .finally(() => {
-        app.listen(port, hostname, () => {
-            console.log(`Server running at http://${hostname}:${port}/`);
+        app.listen(config.port, config.hostname, () => {
+            console.log(`Server running at http://${config.hostname}:${config.port}/`);
         });
     });
