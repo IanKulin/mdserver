256fe45fff2a2e83092a9ad65b3213949b0cc0c7
Ian Bailey <IanKulin@kulin.com.au>
2023-12-17 11:47:40 +0800

    change to multi-architecture

--- docker-build.sh
+++ docker-build.sh
@@ -3,17 +3,5 @@
 # Extract the version number from package.json using jq
 VERSION=$(jq -r .version package.json)
 
-echo docker build --platform linux/amd64 -t iankulin/mdserver:$VERSION -t iankulin/mdserver:latest .
-docker build --platform linux/amd64 -t iankulin/mdserver:$VERSION -t iankulin/mdserver:latest .
-echo docker build --platform linux/arm64 -t iankulin/mdserver:arm64-$VERSION -t iankulin/mdserver:arm64-latest .
-docker build --platform linux/arm64 -t iankulin/mdserver:arm64-$VERSION -t iankulin/mdserver:arm64-latest .
-
-echo docker push iankulin/mdserver:arm64-$VERSION 
-docker push iankulin/mdserver:arm64-$VERSION 
-echo docker push iankulin/mdserver:arm64-latest 
-docker push iankulin/mdserver:arm64-latest 
-
-echo docker push iankulin/mdserver:$VERSION
-docker push iankulin/mdserver:$VERSION
-echo docker push iankulin/mdserver:latest 
-docker push iankulin/mdserver:latest 
+echo docker buildx build --push --platform linux/arm64,linux/amd64 -t iankulin/mdserver:$VERSION -t iankulin/mdserver:latest .
+docker buildx build --push --platform linux/arm64,linux/amd64 -t iankulin/mdserver:$VERSION -t iankulin/mdserver:latest .
